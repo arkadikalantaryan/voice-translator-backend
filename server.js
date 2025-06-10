89a393fc355a5914e4486461ab4795ad69c9d055
arkadikalantaryan <arkadikalantaryan@gmail.com>
2025-06-10 23:08:58 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
