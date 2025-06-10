a932e8e50ecc6d957ae5cdc1e6f3ce7dc9ebf2fe
arkadikalantaryan <arkadikalantaryan@gmail.com>
2025-06-10 23:18:15 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
-const app = express();
+const app = express(); 
 app.use(cors());
 app.use(express.json());
 
